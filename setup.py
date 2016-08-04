dc4a5c4da50d9bff90c863938cd9eedd4a847b06
XavierCLL <xavier.corredor.llano@gmail.com>
2016-08-03 22:59:12 -0500

    Fixes setup file again and added the init package file

--- setup.py
+++ setup.py
@@ -8,38 +8,34 @@
 #
 # License: CC0 1.0 Universal
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 def readme():
     with open("README.md") as f:
         return f.read()
 
-
-setup(name="gfsdownload",
-      version='0.0.3',
-      description="A utility to download ERA INTERIM meteo data ",
-      long_description=readme(),
-      author="Yoann M, Xavier Corredor",
-      author_email="yoann.moreau@gmail.com, xavier.corredor.llano@gmail.com",
-      scripts=["gfsdownload/utils.py", "gfsdownload/eraInterimDownload.py"],
-      url="https://github.com/XavierCLL/gfsDownload",
-      # packages=["gfsdownload"],
-      include_package_data=True,
-      license="CCO",
-      platforms="Posix; ",
-      install_requires=[
-          "GDAL",
-          "OGR",
-          "ecmwfapi"
-      ],
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "Environment :: Console",
-          "Topic :: Scientific/Engineering :: GIS",
-          "Programming Language :: Python",
-      ]
-      )
+setup(
+    name="gfsdownload",
+    version='0.0.3',
+    description="A utility to download GFS and ERA INTERIM meteo data",
+    long_description=readme(),
+    author="Yoann M, Xavier Corredor",
+    author_email="yoann.moreau@gmail.com, xavier.corredor.llano@gmail.com",
+    scripts=["gfsdownload/GFSDownload.py", "gfsdownload/eraInterimDownload.py"],
+    url="https://github.com/XavierCLL/gfsDownload",
+    packages=find_packages(),
+    license="CCO",
+    platforms="Posix; ",
+    install_requires=[
+        "GDAL",
+        "OGR",
+        "ecmwfapi"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Programming Language :: Python",
+    ]
+)
