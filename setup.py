5a8ce6834710362571966a2de3c0db0455c2d3f8
XavierCLL <xavier.corredor.llano@gmail.com>
2016-08-03 22:41:29 -0500

    Fixes setup file

--- setup.py
+++ setup.py
@@ -3,38 +3,43 @@
 # eraInterim Util
 #
 #
-# Author: yoann Moreau
+# Author: yoann Moreau, Xavier Corredor
 # Contributer: benjamin tadry
 #
 # License: CC0 1.0 Universal
-import sys
-import subprocess
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+
 def readme():
     with open("README.md") as f:
         return f.read()
 
-setup(name="eraInterimDownload",
-      version='0.0.1',
+
+setup(name="gfsdownload",
+      version='0.0.3',
       description="A utility to download ERA INTERIM meteo data ",
       long_description=readme(),
-      author="Yoann M",
-      author_email="yoann.moreau@gmail.com",
-      scripts=["gfsdownload/utils.py","gfsdownload/eraInterimDownload.py"],
-      url="https://github.com/yoannMoreau/eraInterimDownload",
-      packages=["gfsdownload"],
+      author="Yoann M, Xavier Corredor",
+      author_email="yoann.moreau@gmail.com, xavier.corredor.llano@gmail.com",
+      scripts=["gfsdownload/utils.py", "gfsdownload/eraInterimDownload.py"],
+      url="https://github.com/XavierCLL/gfsDownload",
+      # packages=["gfsdownload"],
       include_package_data=True,
       license="CCO",
       platforms="Posix; ",
       install_requires=[
           "GDAL",
-          "OGR", 
+          "OGR",
           "ecmwfapi"
       ],
-      classifiers = ["Development Status :: 3 - Alpha"]
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: Console",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Programming Language :: Python",
+      ]
       )
